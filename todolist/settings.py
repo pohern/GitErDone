a3f5b76e833f9e39bd31f6676b38beecb0dde960
pohern <ohern_p@yahoo.com>
2021-02-02 12:28:48 -0600

    add token for github

--- todolist/settings.py
+++ todolist/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': todolist,
+        'NAME': 'todolist',
     }
 }
 
