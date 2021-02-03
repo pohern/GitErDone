c319f66df619a33eaf2ebefd5b66a61084a57f10
pohern <ohern_p@yahoo.com>
2021-02-03 13:26:57 -0600

    migrate

--- todolist/settings.py
+++ todolist/settings.py
@@ -15,8 +15,6 @@ environ.Env.read_env()
 
 import os
 
-
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,8 +80,8 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todolist',
     }
 }
 
