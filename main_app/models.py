a9e1a618f83927f458a8fa4d162bb73fee228c77
pohern <ohern_p@yahoo.com>
2021-02-04 11:49:14 -0600

    add bootstrap to base.html

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,7 @@ class TodoList(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_completed = models.BooleanField()
+    is_completed = models.BooleanField(default=False)
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'todolist_id': self.id})
