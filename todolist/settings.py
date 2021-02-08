68d11b58866ed2b606fea3788bd01cbc9b473d28
pohern <ohern_p@yahoo.com>
2021-02-08 15:42:39 -0600

    Config deployment

--- todolist/settings.py
+++ todolist/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'heroku'
+]
 
 
 # Application definition
